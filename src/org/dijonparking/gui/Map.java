b31fbb3f700c045c6ec11074f3a0dd800358ac1d
LeSuisse <lesuisse.dev@gmail.com>
2011-06-25 01:23:59 +0200

    Ajout Map Pins

--- src/org/dijonparking/gui/Map.java
+++ src/org/dijonparking/gui/Map.java
@@ -18,18 +18,31 @@
 package org.dijonparking.gui;
 
 import greendroid.app.GDMapActivity;
+import greendroid.graphics.drawable.DrawableStateSet;
+import greendroid.graphics.drawable.MapPinDrawable;
 
 import java.util.ArrayList;
 
 import org.dijonparking.R;
 import org.dijonparking.xml.Parking;
 
+import android.content.res.ColorStateList;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.google.android.maps.GeoPoint;
+import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.MapView;
+import com.google.android.maps.OverlayItem;
 
 public class Map extends GDMapActivity {
+	private static final int[] PRESSED_STATE = {android.R.attr.state_pressed};
+	private static final int E6 = (int) Math.pow(10,6);
+	
+	private MapView map;
+	
 	private ArrayList<Parking> parkings;
 	
     /** Called when the activity is first created. */
@@ -38,7 +51,7 @@ public class Map extends GDMapActivity {
         super.onCreate(savedInstanceState);
         setActionBarContentView(R.layout.map);
         
-        final MapView map = (MapView) findViewById(R.id.map);
+        map = (MapView) findViewById(R.id.map);
         map.setBuiltInZoomControls(true);
         map.getController().setZoom(12);
         //Carte centrée sur Dijon au démarrage
@@ -46,12 +59,100 @@ public class Map extends GDMapActivity {
         
         parkings = (ArrayList<Parking>) getIntent().getExtras().get("parkings");
         
-        
+        updateMapPin();
     }
 
 	@Override
 	protected boolean isRouteDisplayed() {
 		return false;
 	}
+	
+	private void updateMapPin() {
+		final Resources r = getResources();
+		
+		for (Parking parking : parkings) {
+			double ratio = -1;
+			
+			if (parking.getNbPlaceDispoTotal() >= 0 && parking.getCapTotale() >= 0)
+				ratio = (double) parking.getNbPlaceDispoTotal() / (double) parking.getCapTotale();
+			
+			BasicItemizedOverlay itemizedOverlay = new BasicItemizedOverlay(new MapPinDrawable(r, getColorStateList(ratio), getColorStateList(ratio)));
+			final GeoPoint pos = new GeoPoint((int) (parking.getLatitude()*E6), (int) (parking.getLongitude()*E6));
 
+			itemizedOverlay.addOverlay(new OverlayItem(pos, parking.getNom(), null));
+			
+			map.getOverlays().add(itemizedOverlay);
+		}
+	}
+	
+	private class BasicItemizedOverlay extends ItemizedOverlay<OverlayItem> {
+
+        private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+
+        public BasicItemizedOverlay(Drawable defaultMarker) {
+            super(boundCenterBottom(defaultMarker));
+        }
+
+        public void addOverlay(OverlayItem overlay) {
+            mOverlays.add(overlay);
+            populate();
+        }
+
+        @Override
+        protected OverlayItem createItem(int i) {
+            return mOverlays.get(i);
+        }
+
+        @Override
+        public int size() {
+            return mOverlays.size();
+        }
+
+        @Override
+        protected boolean onTap(int index) {
+            return true;
+        }
+
+    }
+	
+	private ColorStateList getColorStateList(double ratio) {
+		int[][] states = new int[2][];
+        int[] colors = new int[2];
+
+        final int color = getColor(ratio);
+
+        states[0] = PRESSED_STATE;
+        colors[0] = addRGB(color, -50);
+
+        states[1] = DrawableStateSet.EMPTY_STATE_SET;
+        colors[1] = color;
+
+        return new ColorStateList(states, colors);
+	}
+	
+	private static final int getColor(double ratio) {
+		int color;
+		
+		if (ratio < 0)
+			color = Color.GRAY;
+		else if (ratio < 0.2)
+			color = Color.RED;
+		else if (ratio < 0.4)
+			color = Color.YELLOW;
+		else
+			color = Color.GREEN;
+		
+		return color;
+	}
+
+	private static int addRGB(int color, int amount) {
+        int r = constrain(Color.red(color) + amount, 0, 255);
+        int g = constrain(Color.green(color) + amount, 0, 255);
+        int b = constrain(Color.blue(color) + amount, 0, 255);
+        return Color.rgb(r, g, b);
+    }
+	
+	private static int constrain(int amount, int low, int high) {
+        return amount < low ? low : (amount > high ? high : amount);
+    }
 }
